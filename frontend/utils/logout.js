b68303914291bfe54525c3566349a5d7b818c121
Disha Shukla <dishashukla0110@gmail.com>
2025-07-12 01:54:44 +0530

    Update logout.js

--- frontend/utils/logout.js
+++ frontend/utils/logout.js
@@ -1,7 +1,7 @@
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const logout = async () => {
-  const res = await fetch('http://localhost:8080/api/logout', {
+  const res = await fetch(`${BASE_URL}/api/logout`, {
     method: 'POST',
     credentials: 'include',
   });
